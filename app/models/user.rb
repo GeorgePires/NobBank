739984b3d8d19e43df55f3c5a3ca271003b38490
GeorgePires <georgeasp.pi@gmail.com>
2023-03-31 22:49:03 -0300

    :sparkles: feat: set views devise for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  validates :first_name, :last_name, presence: true
+  validates :first_name, length: { minimum: 3 }
+
+  def full_name
+    "#{self.first_name} #{self.last_name}"
+  end
 end
