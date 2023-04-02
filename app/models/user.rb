09eaacac619d1e4ab515f3279009d90764114f1f
GeorgePires <georgeasp.pi@gmail.com>
2023-04-01 22:56:10 -0300

    :sparkles: feat: add callback create_account

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  has_many :accounts, dependent: :destroy
+  after_create :create_account
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -9,5 +12,12 @@ class User < ApplicationRecord
 
   def full_name
     "#{self.first_name} #{self.last_name}"
+  end
+
+  def create_account
+    account_number = (Digest::SHA1.hexdigest(SecureRandom.uuid)[0..7]).upcase
+    unless Account.exists?(account_number: account_number)
+      Account.create!(user_id: self.id, balance: 0.00, account_number: account_number, status: true)
+    end
   end
 end
