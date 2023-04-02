fe863e48f30cc00c499993225180233e5ceac7cd
GeorgePires <georgeasp.pi@gmail.com>
2023-04-01 22:30:14 -0300

    :sparkles: feat: create transaction and account controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'transactions/index'
+  get 'transactions/show'
+  get 'accounts/index'
+  get 'accounts/new'
+  get 'accounts/create'
+  get 'accounts/edit'
+  get 'accounts/update'
+  get 'accounts/show'
   get 'welcome/index'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
