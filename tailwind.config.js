340c3334ad1a4144b75107f34f2325241d191850
GeorgePires <georgeasp.pi@gmail.com>
2023-04-02 14:24:00 -0300

    :heavy_plus_sign: build: add flowbite

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,14 @@ module.exports = {
   plugins: [
     // ...
     require('@tailwindcss/forms'),
-    require('daisyui')
+    require('daisyui'),
+    require('flowbite/plugin')
   ],
   content: [
     './app/views/**/*.html.erb',
     './app/helpers/**/*.rb',
     './app/assets/stylesheets/**/*.css',
-    './app/javascript/**/*.js'
+    './app/javascript/**/*.js',
+    './node_modules/flowbite/**/*.js'
   ]
 }
