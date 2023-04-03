3459aed89aa9d19da7bdf88ccafae973bed7b536
GeorgePires <georgeasp.pi@gmail.com>
2023-04-03 18:58:04 -0300

    :lipstick: style: set views sign_up and sign_in

--- config/routes.rb
+++ config/routes.rb
@@ -8,9 +8,7 @@ Rails.application.routes.draw do
   get 'accounts/update'
   get 'accounts/show'
   get 'welcome/index'
+  
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root 'welcome#index'
 end
