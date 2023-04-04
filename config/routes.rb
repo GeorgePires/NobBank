33308b4d3eea0a0a55bc2f47954683d1372db9d2
GeorgePires <georgeasp.pi@gmail.com>
2023-04-03 22:42:19 -0300

    :heavy_plus_sign: build: set routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,10 @@
 Rails.application.routes.draw do
   get 'transactions/index'
   get 'transactions/show'
-  get 'accounts/index'
-  get 'accounts/new'
-  get 'accounts/create'
-  get 'accounts/edit'
-  get 'accounts/update'
-  get 'accounts/show'
+
   get 'welcome/index'
   
   devise_for :users
   root 'welcome#index'
+  resources :accounts
 end
