b0b1bd078c3f65fc8b7c0da6679e1ed533a6ff59
GeorgePires <georgeasp.pi@gmail.com>
2023-04-04 21:46:54 -0300

    :lipstick: style: add method color_status

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def color_status(status)
+    color = status == true ? 'green' : 'red'
+    "px-2 py-1 font-semibold leading-tight text-#{color}-700 bg-#{color}-100 rounded-full"
+  end
 end
