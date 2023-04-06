1fa67e6f08f5f6a99cdb8ac5612a8fd235f2a496
GeorgePires <georgeasp.pi@gmail.com>
2023-04-06 02:02:04 -0300

    :sparkles: feat: create helper color_transaction

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,14 @@ module ApplicationHelper
     color = status == true ? 'green' : 'red'
     "px-2 py-1 font-semibold leading-tight text-#{color}-700 bg-#{color}-100 rounded-full"
   end
+
+  def color_transaction(transaction_type)
+    if transaction_type == 'credit'
+      "px-2 py-1 font-semibold leading-tight text-green-700 bg-green-100 rounded-full"
+    elsif transaction_type == 'debit'
+      "px-2 py-1 font-semibold leading-tight text-red-700 bg-red-100 rounded-full"
+    else
+      "px-2 py-1 font-semibold leading-tight text-blue-700 bg-blue-100 rounded-full"
+    end
+  end
 end
