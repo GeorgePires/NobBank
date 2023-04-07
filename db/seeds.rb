fcfe10a372dcf057ccda71d58e3296c1a0bd0716
GeorgePires <georgeasp.pi@gmail.com>
2023-04-06 22:35:34 -0300

    :heavy_plus_sign: build: set seed and add dev setup

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+# Hi!, use the rails dev:setup command to create data.
+
+# Create Users
+PASSWORD = 123_246
+george = User.create!(first_name: "George", last_name: "Pires", email: "george@gmail.com", password: PASSWORD, password_confirmation: PASSWORD)
+wes = User.create!(first_name: "Wes", last_name: "Pires", email: "wes@gmail.com", password: PASSWORD, password_confirmation: PASSWORD)
+
+# Create Deposit and Withdraw
+george_account = george.accounts.first
+george_account.deposit(2000)
+george_account.withdraw(200)
+
+# Create transfer from George account to Wes account
+wes_account = wes.accounts.first
+george_account.transfer(1100, wes_account)
