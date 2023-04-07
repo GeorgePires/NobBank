531de2b9d58c130f3203fe7bebf652f9cf452ab3
GeorgePires <georgeasp.pi@gmail.com>
2023-04-06 21:36:45 -0300

    :sparkles: feat: create helper active_button_text

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,10 +4,14 @@ module ApplicationHelper
     "px-2 py-1 font-semibold leading-tight text-#{color}-700 bg-#{color}-100 rounded-full"
   end
 
+  def active_button_text(status)
+    status == true ? 'Suspended' : 'Active'
+  end
+
   def color_transaction(transaction_type)
-    if transaction_type == 'credit'
+    if transaction_type == 'deposit'
       "px-2 py-1 font-semibold leading-tight text-green-700 bg-green-100 rounded-full"
-    elsif transaction_type == 'debit'
+    elsif transaction_type == 'withdraw'
       "px-2 py-1 font-semibold leading-tight text-red-700 bg-red-100 rounded-full"
     else
       "px-2 py-1 font-semibold leading-tight text-blue-700 bg-blue-100 rounded-full"
