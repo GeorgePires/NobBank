97e8388b0b97435e76b2dd14df455425626e1ce2
GeorgePires <georgeasp.pi@gmail.com>
2023-05-17 23:08:33 -0300

    :recycle: refactor: account controller

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -2,43 +2,28 @@
 
 class AccountsController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_account, only: %i[show destroy]
-  before_action :set_account_and_user,
-                only: %i[deposit create_deposit withdraw create_withdraw transfer create_transfer]
+  before_action :set_account_with_user
 
   def index
-    @accounts = current_user.accounts
-    @total_accounts_balance = current_user.accounts.sum(:balance)
-    @transactions = Transaction.all
+    @total_accounts_balance = current_user.account.balance
+    @transactions = current_user.account.transactions.history_transactions.limit(8)
   end
 
   def show
     @transactions = Transaction.where(account_id: @account.id).history_transactions
   end
 
-  def new
-    @account = current_user.accounts.build
-  end
-
-  def create
-    @account = current_user.accounts.build(account_params)
-    if @account.save
-      redirect_to accounts_path, notice: 'Account was successfully created.'
-    else
-      render :new
-    end
-  end
-
   def destroy
-    if @account.status == false
-      @account.update(status: true)
-      redirect_to accounts_path, notice: 'Account was successfully active'
-    else
-      @account.update(status: false)
+    if @account.status?
+      @account.update!(status: false)
       redirect_to accounts_path, alert: 'Account was successfully suspended'
+    else
+      @account.update!(status: true)
+      redirect_to accounts_path, notice: 'Account was successfully active'
     end
   end
 
+  # GET
   def deposit; end
   def withdraw; end
   def transfer; end
@@ -63,14 +48,16 @@ class AccountsController < ApplicationController
 
   def create_transfer
     amount = params[:amount].to_d
-    account = Account.find_by(account_number: params[:account_number])
-    if account.present?
+    destiny_account = Account.find_by(account_number: params[:account_number])
+
+    if destiny_account.present? && destiny_account.status?
       begin
-        if @account.transfer(amount, account)
-          redirect_to accounts_path, notice: "Transfer of $ #{amount} to #{account.account_number} was successful."
+        if @account.transfer(amount, destiny_account)
+          redirect_to accounts_path,
+          notice: "Transfer of $ #{amount} to #{destiny_account.account_number} was successful."
         end
       rescue StandardError => e
-        redirect_to accounts_path, alert: "Transfer to #{account.account_number}: #{e.message}"
+        redirect_to accounts_path, alert: "Transfer to #{destiny_account.account_number}: #{e.message}"
       end
     else
       redirect_to accounts_path, alert: 'Account not found'
@@ -79,21 +66,11 @@ class AccountsController < ApplicationController
 
   private
 
-  def set_account
-    @account = Account.find(params[:id])
-  end
-
-  def set_account_and_user
-    @user = current_user
-    @account = @user.accounts.find(params[:account_id])
+  def set_account_with_user
+    @account = current_user.account
   end
 
   def account_params
-    params.require(:account).permit(:balance, :status).merge(account_number: generate_account_number)
-  end
-
-  def generate_account_number
-    account_number = SecureRandom.uuid[0..7].upcase
-    return account_number unless Account.exists?(account_number:)
+    params.require(:account).permit(:balance, :status, :account_number)
   end
 end
