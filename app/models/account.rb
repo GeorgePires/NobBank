7747ba082048848fd2ef2b1e77b14cd6c93e28cd
GeorgePires <georgeasp.pi@gmail.com>
2023-05-17 23:10:17 -0300

    :sparkles: feat: create validations for transactions

--- app/models/account.rb
+++ app/models/account.rb
@@ -7,10 +7,12 @@ class Account < ApplicationRecord
   validates :balance, presence: true
   validates :account_number, presence: true, uniqueness: true
 
-  # before_validation :check_status
+  # enum status: {}
+
+  validates :status, acceptance: { message: 'Invalid account' }, unless: :will_save_change_to_status?
 
   def deposit(amount)
-    raise 'Invalid amount' unless amount.positive?
+    amount_positive?(amount)
 
     ActiveRecord::Base.transaction do
       self.balance += amount
@@ -21,8 +23,7 @@ class Account < ApplicationRecord
   end
 
   def withdraw(amount)
-    raise 'Invalid amount' unless amount.positive?
-    raise 'Insufficient funds' unless self.balance >= amount
+    amount_positive?(amount) || validate_amount(amount)
 
     ActiveRecord::Base.transaction do
       self.balance -= amount
@@ -32,22 +33,34 @@ class Account < ApplicationRecord
     end
   end
 
-  def transfer(amount, account)
-    raise 'Insufficient funds' unless self.balance >= amount && id != account.id
-
+  def transfer(amount, destiny_account)
+    amount_positive?(amount) || validate_amount(amount) || validate_account_id(destiny_account)
     ActiveRecord::Base.transaction do
       total_amount = calc_transfer_fee(amount)
       raise 'Insufficient funds' unless self.balance >= total_amount
 
-      update(balance: self.balance -= total_amount)
-      account.update(balance: account.balance += amount)
-      transaction = transactions.create!(destiny_account: account.account_number, transaction_type: 'transfer',
-                                         amount:)
+      update!(balance: self.balance -= total_amount)
+      destiny_account.update!(balance: destiny_account.balance += amount)
+      Transaction.create!(transaction_type: 'transfer received', amount:, account_id: destiny_account.id)
+      transaction = transactions.create!(destiny_account: destiny_account.account_number,
+                                         transaction_type: 'transferred', amount:)
       transaction
     end
   end
 
   private
+
+  def amount_positive?(amount)
+    raise 'Invalid amount' unless amount.positive?
+  end
+
+  def validate_amount(amount)
+    raise 'Insufficient funds' unless self.balance >= amount
+  end
+
+  def validate_account_id(destiny_account)
+    raise 'Destination account must be different from source account' unless id != destiny_account.id
+  end
 
   def calc_transfer_fee(amount)
     total_amount = if [0, 6].exclude?(Time.current.wday) && (Time.zone.now.hour >= 9 && Time.zone.now.hour < 18)
@@ -58,10 +71,4 @@ class Account < ApplicationRecord
     total_amount += 10 if amount > 1000
     total_amount
   end
-
-  # def check_status
-  #  unless self.status
-  #    raise "Invalid account status"
-  #  end
-  # end
 end
