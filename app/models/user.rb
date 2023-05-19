cd3c24c15657cb704fb08fc0de66220325f474c3
GeorgePires <georgeasp.pi@gmail.com>
2023-05-19 00:04:26 -0300

    :sparkles: feat: add has_one to account

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  has_many :accounts, dependent: :destroy
+  has_one :account, dependent: :destroy
 
   after_create :create_account
 
